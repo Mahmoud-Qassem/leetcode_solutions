7c792207d44d8c6ff86bb736640e12bca90a909c
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2023-03-22 01:20:06 +0200

    Time: 32 ms (50.73%), Space: 11.9 MB (68.14%) - LeetHub

--- coin-change-ii/coin-change-ii.cpp
+++ coin-change-ii/coin-change-ii.cpp
@@ -1,38 +1,26 @@
-vector<int>v;
-int n,c=0;
-const long long MX=5005,MXLAST=305;
-long long dp[MX][MXLAST];
-class Solution
-{
-
-private:
-    long long solve(long long x, int last)
-    {
-        if (x < 0) return 0;
-        if (x == 0)return 1;
-
-
-        auto&ret=dp[x][last];
-        if(~ret)return ret;
-
-
-        long long best = 0;
-
-
-        for(int i=last; i<n; ++i)
-        {
-            best+=solve(x-v[i],i);
-        }
-        return ret=best;
-    }
-
-
+class Solution {
 public:
-    int change(int amount,vector<int>& coins)
-    {
-        v=coins;
-        n=v.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(amount,0);
+    int change(int amount, vector<int>& coins) {
+        // vector<vector<int>>dp(amount+1,vector<int>(coins.size()+1));
+        int dp[amount+1][coins.size()+1];
+        dp[0][0]=0;
+        for(int i=1;i<=coins.size();i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<=amount;i++){
+            dp[i][0]=0;
+        }
+
+        //dp[i][j] represents the the no. of ways to find sum i with the element lengths of j of coins
+        for(int i=1;i<=amount;i++){
+            for(int j=1;j<=coins.size();j++){
+                dp[i][j]=dp[i][j-1];//not to include current element
+                if(coins[j-1]<=i){
+                    dp[i][j]+=dp[i-coins[j-1]][j];//include current element
+                }
+            }
+        }
+        return dp[amount][coins.size()];
+        
     }
 };
