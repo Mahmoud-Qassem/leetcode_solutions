d29a2a98b587196a66435173d00bfe5715049dd0
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2023-03-26 15:39:26 +0200

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- 3sum-closest/3sum-closest.cpp
+++ 3sum-closest/3sum-closest.cpp
@@ -1,35 +1,32 @@
+#include <vector>
 #include <algorithm>
 #include <climits>
-#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    int threeSumClosest(std::vector<int>& nums, int target) {
-        std::sort(nums.begin(), nums.end());
-        int closestSum = INT_MAX;
-        int closestDiff = INT_MAX;
-        
-        for (int i = 0; i < nums.size() - 2; ++i) {
-            int j = i + 1;
-            int k = nums.size() - 1;
-            
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                int diff = std::abs(sum - target);
-                
-                if (diff < closestDiff) {
-                    closestSum = sum;
-                    closestDiff = diff;
+    int threeSumClosest(vector<int>& v, int target) {
+        sort(v.begin(), v.end()); // Sort the vector to enable two-pointer approach
+        int n = v.size();
+        long long closest_sum = INT_MAX;
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                long long sum = v[i] + v[left] + v[right];
+                if (abs(sum - target) < abs(closest_sum - target)) {
+                    closest_sum = sum;
                 }
-                
                 if (sum < target) {
-                    ++j;
-                } else {
-                    --k;
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else { // sum == target
+                    return target;
                 }
             }
         }
-        
-        return closestSum;
+        return closest_sum;
     }
 };
