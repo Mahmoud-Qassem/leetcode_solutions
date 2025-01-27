11f246fd6ceb9fba9cc9bba241e4f8c5ff14c8e5
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-01-27 14:49:18 +0200

    Time: 0 ms (100%), Space: 24.1 MB (41.24%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& v, int n) {
+        int m = v.size();
+        if (m == 1 and n and v[0])
+            return 0;
+        else if (m == 1)
+            return 1;
+
+        for (int i = 0; i < m and n; ++i) {
+            if (!v[i]) {
+                if (!i and !v[i + 1]) {
+                    v[i] = 1;
+                    n--;
+                }
+                else if (i and !v[i - 1] and i == m - 1) {
+                    v[i] = 1;
+                    n--;
+                } else if (i and i < m - 1 and !v[i - 1] and !v[i + 1]) {
+                    v[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return !n;
+    }
+};
