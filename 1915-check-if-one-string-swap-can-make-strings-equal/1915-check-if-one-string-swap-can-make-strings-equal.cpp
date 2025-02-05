619ff73b516e8aed5765ca1289bab63a6a3654bf
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-02-05 02:16:50 +0200

    Time: 7 ms (5.04%), Space: 8.5 MB (36.33%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         int sz=s1.size();
+        int sz2=s1.size();
+        if(sz!=sz2)return false;
         for(int i=0;i<sz;++i){
             for(int j=0;j<sz;++j){
                 swap(s1[i],s1[j]);
