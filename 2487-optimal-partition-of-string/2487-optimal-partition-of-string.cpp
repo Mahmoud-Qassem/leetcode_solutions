93c46818a38990b9b3b402b032fae89979d3dcfe
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-02-06 00:59:35 +0200

    Time: 43 ms (59.65%), Space: 23.7 MB (57.71%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.cpp
@@ -3,13 +3,13 @@ public:
     int partitionString(string s) {
         s+=s.back();
         int n=s.size(),cnt=0;
-        vector<int>frq(27);
+        vector<bool>frq(27);
         for(int i=0;i<n;++i){
             if(frq[s[i]-'a']){
                 cnt++;
-                frq=vector<int>(27);
+                frq=vector<bool>(27);
             }
-            frq[s[i]-'a']++;
+            frq[s[i]-'a']=true;
         }
         return cnt;
     }
