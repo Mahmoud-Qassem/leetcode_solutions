8c7a661f0d8bf35998b0fa9f2be45d0735aa8726
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-02-15 03:54:34 +0200

    Time: 2 ms (89.67%), Space: 8.1 MB (82.37%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -2,34 +2,13 @@ class Solution {
 public:
     int punishmentNumber(int n) {
         int sum=0;
-        auto check=[&](int i)-> bool{
-            string s=to_string(i*i);
-            int sz=s.size();
-            for(int msk=0,tmp,crnt=0;msk<(1<<sz);++msk,crnt=0){
-                tmp=msk;
-                bool lst=tmp%2;
-                string sub;
-                
-                for(int j=0;j<sz;++j,lst=tmp%2,tmp/=2){
-                    if(tmp%2==lst){
-                        sub+=s[j];
-                    }
-                    else{
-                        crnt+=stoi(sub);
-                        sub="";
-                        sub+=s[j];
-                    }
-                    if(j==sz-1)
-                        crnt+=stoi(sub);
-                }
-                if(crnt==i)return 1;
-            }
-            return 0;
-            
-        };
-        
+        vector<int>valid={1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000};
+
+        vector<bool>check(1001);
+        for(auto&it:valid)check[it]=true;
+
         for(int i=1;i<=n;++i){
-            if(check(i))sum+=i*i;
+            if(check[i])sum+=i*i;
         }
         return sum;
     }
