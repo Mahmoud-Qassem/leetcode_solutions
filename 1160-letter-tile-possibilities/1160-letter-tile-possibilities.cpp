8a8548b48562b8b8ffd43776e48311f93c4027c6
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-02-18 00:04:19 +0200

    Time: 3 ms (78.92%), Space: 7.7 MB (97.76%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
-        set<string> st;
-        int n = tiles.size();
-        sort(tiles.begin(), tiles.end());
-        do {
-            for (int i = 0; i < (1 << n); ++i) {
-                int msk = i;
-                string s;
-                for (int j = 0; j < n; ++j, msk /= 2) {
-                    if (msk % 2)
-                        s += tiles[j];
-                }
-                st.insert(s);
+    int a = 0;
+    void c(vector<int>&f){
+        for(int i = 0;i<26;i++){
+            if(f[i]>0){
+                f[i]--;
+                a++;
+                c(f);
+                f[i]++;
             }
-
-        } while (next_permutation(tiles.begin(), tiles.end()));
-        for(auto&it:st)cout<<it<<" ";cout<<endl;
-        return (int)st.size()-1;
+        }
+    }
+    int numTilePossibilities(string tiles) {
+        vector<int>f(26,0);
+        for(auto ch : tiles)f[ch -'A']++;
+        c(f);
+        return a;
     }
 };
