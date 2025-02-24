6dbf05f75a1864d4f6369ba5e7f15b72b5e7812f
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-02-24 03:19:24 +0200

    Time: 656 ms (87.85%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
-SELECT product_id FROM  products
-WHERE low_fats = 'Y' AND recyclable ='Y';
+/* Write your T-SQL query statement below */
+select product_id from products
+where low_fats = 'Y' and recyclable ='Y'
