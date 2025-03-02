1b2c3b69e648b7bfa49a1ba4844021adc293dbb2
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-02 06:50:02 +0200

    Time: 0 ms (100%), Space: 15.2 MB (28.71%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        map<int, int> mergedMap;
-        // Insert values from the first array into the map
+        vector<int> mergedMap(2005, 0);
+        
+        // Insert values from the first array into the vector
         for (const auto& pair : nums1) {
             mergedMap[pair[0]] += pair[1];
         }
-        // Insert values from the second array into the map
+        // Insert values from the second array into the vector
         for (const auto& pair : nums2) {
             mergedMap[pair[0]] += pair[1];
         }
-        // Convert the map to the resulting vector of vectors
+        // Convert the vector to the resulting vector of vectors
         vector<vector<int>> result;
-        for (const auto& [id, val] : mergedMap) {
-            result.push_back({id, val});
+        for (int i = 0; i < mergedMap.size(); ++i) {
+            if (mergedMap[i]) {
+                result.push_back({i, mergedMap[i]});
+            }
         }
         return result;
     }
