2999f723afc2ff6bbb3c84e313e3f55ec3f9893e
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-05 01:23:46 +0200

    Time: 0 ms (100%), Space: 8 MB (9.2%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -3,9 +3,8 @@ public:
     bool checkPowersOfThree(int n) {
         int x=15;
         while(x--){
-            int p=(int)(pow(3,x));
-            if(p<=n){
-                n-=p;
+            if((int)(pow(3,x))<=n){
+                n-=(int)(pow(3,x));
             }
         }
         return !n;
