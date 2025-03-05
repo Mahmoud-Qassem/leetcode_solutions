f835ee3065456ed1caf7c136097c985451461d41
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-05 17:17:22 +0200

    Time: 1050 ms (15.64%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,3 +1,3 @@
-Select distinct(v1.author_id) as id
-from views v1 , views v2 where v2.author_id=v1.viewer_id and v1.article_id=v2.article_id
-order by v1.author_id
+Select distinct(author_id) as id
+from views where author_id=viewer_id 
+order by author_id
