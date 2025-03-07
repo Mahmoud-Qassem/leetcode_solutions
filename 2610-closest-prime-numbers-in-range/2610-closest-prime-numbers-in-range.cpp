5badedfc7a41373b88c4308a40c8fbc5d7da0d33
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-07 02:50:23 +0200

    Time: 15 ms (88.7%), Space: 9.1 MB (87.22%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,40 +1,33 @@
-vector<int>primes;
-const int MAX=1e6+5;
-bool isprime[MAX];
-bool done=0;
-void init(){
-    if(done)return ;
-    done=1;
+bool isPrime[1000005] = {false};
 
-    for(int  i=0;i<MAX;++i)isprime[i]=1;
-    isprime[0]=isprime[1]=0;
-
-    for(int i=2;i*i<MAX;++i){
-        if(isprime[i]){
-
-            for(int j=i*2;j<MAX;j+=i)isprime[j]=0;
-        }
-    }
-    for(int i=0;i<MAX;++i)if(isprime[i])primes.push_back(i);
-
-}
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
-        init();
-        int a = -1, b = -1;
-        int a_idx=lower_bound(primes.begin(),primes.end(),left)-primes.begin();
-        int b_idx=upper_bound(primes.begin()+a_idx+1,primes.end(),right)-primes.begin();
-        
-        int diff=INT_MAX;
-        for(int i=a_idx;i<b_idx-1;++i){
-            //cout<<primes[i]<<" ";
-            if(primes[i+1]-primes[i]<diff){
-                diff=primes[i+1]-primes[i];
-                a=primes[i];
-                b=primes[i+1];
-            }
+        if (isPrime[2] == 0) {
+            memset(isPrime, 1, sizeof(isPrime));
+            static int arr[2];
+            isPrime[1] = 0;
+
+            for (int i = 2; i*i <= 1000000; ++i)
+                if (isPrime[i])
+                    for (int j = i << 1; j <= 1000000; j += i)
+                        isPrime[j] = false;
         }
-        return {a, b};
+
+        int idx = 0;
+        vector<int> res{-1, -1};
+
+        for (int i = left, last = -1; i <= right ; ++i)
+            if (isPrime[i]) {
+                if (last != -1) {
+                    if (res[0] == -1)
+                        res[1] = i, res[0] = last;
+                    else if(res[1] - res[0] > i - last)
+                        res[1] = i, res[0] = last;
+                }
+                last = i;
+            }
+        
+        return res;
     }
 };
