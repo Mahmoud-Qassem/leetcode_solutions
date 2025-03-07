ca5883012aa6bb59b7b83155aed5a20e99641d7f
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-07 02:48:38 +0200

    Time: 4 ms (90.74%), Space: 10.3 MB (82.59%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,28 +1,27 @@
+vector<int>primes;
+const int MAX=1e6+5;
+bool isprime[MAX];
+bool done=0;
+void init(){
+    if(done)return ;
+    done=1;
 
+    for(int  i=0;i<MAX;++i)isprime[i]=1;
+    isprime[0]=isprime[1]=0;
+
+    for(int i=2;i*i<MAX;++i){
+        if(isprime[i]){
+
+            for(int j=i*2;j<MAX;j+=i)isprime[j]=0;
+        }
+    }
+    for(int i=0;i<MAX;++i)if(isprime[i])primes.push_back(i);
+
+}
 class Solution {
 public:
-    const int MAX=1e6+5;
-    vector<int>primes;
-    Solution() {
-        vector<bool>isprime(MAX);
-
-        for (int i = 0; i < MAX; ++i)
-            isprime[i] = 1;
-        isprime[0] = isprime[1] = 0;
-
-        for (int i = 2; i * i < MAX; ++i) {
-            if (isprime[i]) {
-
-                for (int j = i * 2; j < MAX; j += i)
-                    isprime[j] = 0;
-            }
-        }
-        for (int i = 0; i < MAX; ++i)
-            if (isprime[i])
-                primes.push_back(i);
-    }
-
     vector<int> closestPrimes(int left, int right) {
+        init();
         int a = -1, b = -1;
         int a_idx=lower_bound(primes.begin(),primes.end(),left)-primes.begin();
         int b_idx=upper_bound(primes.begin()+a_idx+1,primes.end(),right)-primes.begin();
