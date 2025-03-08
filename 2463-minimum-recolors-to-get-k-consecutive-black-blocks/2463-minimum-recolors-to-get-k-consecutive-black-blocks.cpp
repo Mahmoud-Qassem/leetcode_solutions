50588e4a5bae9c1554194b43d06cd59d378a773d
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-08 06:06:18 +0200

    Time: 3 ms (6.58%), Space: 8.2 MB (92.08%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int minimumRecolors(string s, int k) {
-        int n=s.size();
-        int mn=k;
-        for(int i=0;i<n;++i){
-            int w=0,b=0;
-            for(int j=i;j<n;++j){
-                w+=(s[j]=='W');
-                b+=(s[j]=='B');
-                if(j-i+1==k){
-                    mn=min(mn,max(k-b,0));
-                    break;
-                }
-            }
+        int left=0,right=0,w=0,b=0,n=s.size();
+        while(right-left+1<=k){
+            w+=(s[right]=='W');
+            b+=(s[right]=='B');
+            right++;
+        }
+        int mn=max(k-b,0);
+        cout<<mn<<endl;
+        while(right<n){
+            w-=(s[left]=='W');
+            b-=(s[left]=='B');
+            left++;
+            w+=(s[right]=='W');
+            b+=(s[right]=='B');
+            if(right-left+1==k)
+            mn=min(mn,max(k-b,0));
+            right++;
+            cout<<mn<<endl;
         }
         return mn;
     }
