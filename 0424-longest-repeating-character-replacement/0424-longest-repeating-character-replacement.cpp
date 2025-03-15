4766c0f389e4d24be4a152d5170994eca5647ebf
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-15 09:26:02 +0200

    Time: 19 ms (13.97%), Space: 10.9 MB (73.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,14 +4,14 @@ public:
         int n = s.size(), ans = 0, left = 0, right = 0;
         vector<int> frq(26);
         while (left < n) {
-            while (right < n and
-                   (right - left) - *max_element(frq.begin(), frq.end()) <= k) {
-                frq[s[right++]-'A']++;
+            while (right < n) {
+                frq[s[right++] - 'A']++;
                 if ((right - left) - *max_element(frq.begin(), frq.end()) <= k)
                     ans = max(ans, right - left);
+                else
+                    break;
             }
-            //cout << left << " " << right << endl;
-            frq[s[left++]-'A']--;
+            frq[s[left++] - 'A']--;
         }
         return ans;
     }
