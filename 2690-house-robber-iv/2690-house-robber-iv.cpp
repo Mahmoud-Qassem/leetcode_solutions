c468f583baccadf5eeedc77186ad3be74e1f848c
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-15 06:37:01 +0200

    Time: 20 ms (75.32%), Space: 61 MB (40.42%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int k, int limit){
-        int n=nums.size();
-        for(int i=0;i<n and k;++i){
-            if(nums[i]<=limit){
+    bool check(vector<int>& nums, int k, int limit) {
+        int n = nums.size();
+        for (int i = 0; i < n and k; ++i) {
+            if (nums[i] <= limit) {
                 i++;
                 k--;
             }
@@ -11,16 +11,16 @@ public:
         return not k;
     }
     int minCapability(vector<int>& nums, int k) {
-        int n=nums.size();
-        int left=1, right=1e9+1,ans =0;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(check(nums,k,mid)){
-                right=mid-1;
-                ans=mid;
-            }
-            else {
-                left=mid+1;
+        int n = nums.size(), ans;
+        int left = *min_element(nums.begin(), nums.end());
+        int right = *max_element(nums.begin(), nums.end());
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (check(nums, k, mid)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
             }
         }
         return ans;
