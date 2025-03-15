d73ab8914c5e8f30a7dccefabc41c0c5ab78d538
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-15 10:28:14 +0200

    Time: 10 ms (54.46%), Space: 11.3 MB (81.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string minWindow(string big, string small) {
-        int n=big.size(), m=small.size();
-        if(n<m)return "";
+        int n = big.size(), m = small.size();
+        if (n < m)
+            return "";
 
-        vector<int>crnt(128),fixed(128);
-        for(char c:small)fixed[c]++;
-        auto valid=[&]()-> bool{
-            for(int i=0;i<128;++i){
-                if(fixed[i] and fixed[i]>crnt[i]){
+        vector<int> crnt(128), fixed(128);
+        for (char c : small)
+            fixed[c]++;
+        auto valid = [&]() -> bool {
+            for (int i = 0; i < 128; ++i) {
+                if (fixed[i] and fixed[i] > crnt[i]) {
                     return false;
                 }
             }
             return true;
         };
-        int left=0,right=0,ans=INT_MAX,x=-1,y=-1;
-        while(right<n){
+        int left = 0, right = 0, ans = INT_MAX, x = -1, y = -1;
+        while (right < n) {
             crnt[big[right]]++;
             right++;
-            
-            while(left<n and fixed[ big[left] ]< crnt[big[left]]){
+
+            while (left < n and fixed[big[left]] < crnt[big[left]]) {
                 crnt[big[left]]--;
                 left++;
             }
-            if(valid()){
-                if(ans>right-left+1){
-                    x=left,y=right,ans=right-left+1;
-                }
+
+            if (ans > right - left + 1 and valid()) {
+                x = left, y = right, ans = right - left + 1;
             }
         }
-        if(x!=-1)return big.substr(x,y-x);
+        if (x != -1)
+            return big.substr(x, y - x);
         return "";
     }
 };
