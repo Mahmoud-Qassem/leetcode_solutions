65b943455a9a48d8e0868284584afb6b90b52c40
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-15 06:41:59 +0200

    Time: 15 ms (95.45%), Space: 60.9 MB (67.69%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,27 +1,35 @@
 class Solution {
-    bool check(vector<int>& nums, int k, int limit) {
-        int n = nums.size();
-        for (int i = 0; i < n and k and (n - i) >= k; ++i) {
-            if (nums[i] <= limit) {
-                i++;
+    bool isPossible(vector<int> &arr, int k, int maxi) {
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] <= maxi) {
+                i += 2;
                 k--;
+            } else {
+                i++;
+            }
+
+            if(k == 0) {
+                return true;
             }
         }
-        return not k;
+        return false;
     }
-
 public:
-    int minCapability(vector<int>& nums, int k) {
-        int n = nums.size(), ans;
-        int left = *min_element(nums.begin(), nums.end());
-        int right = *max_element(nums.begin(), nums.end());
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(nums, k, mid)) {
-                right = mid - 1;
+    int minCapability(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = *min_element(arr.begin(), arr.end());
+        int high = *max_element(arr.begin(), arr.end());
+
+        int ans = -1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(isPossible(arr, k, mid)) {
                 ans = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
         return ans;
