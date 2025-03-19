7904cd64dbc8ca176486671c26f84b12cad870d7
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-03-19 05:18:49 +0200

    Time: 11 ms (31.84%), Space: 140 MB (74.5%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -4,8 +4,8 @@ public:
         int cnt=0,n=nums.size();
         for(int i=0;i<n-2;++i){
             if(!nums[i]){
-                nums[i+1]=!nums[i+1];
-                nums[i+2]=!nums[i+2];
+                nums[i+1]^=1;
+                nums[i+2]^=1;
                 cnt++;
             }
         }
