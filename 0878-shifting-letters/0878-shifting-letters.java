514ce8af14bfe432ced931e5ee3c5e1865d8232b
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-09-29 17:07:37 +0300

    Time: 6 ms (91.98%), Space: 61.3 MB (7.59%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.java
+++ 0878-shifting-letters/0878-shifting-letters.java
@@ -1,13 +1,14 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
+        long[] longShifts = new long[shifts.length];
+        longShifts[shifts.length-1] = shifts[shifts.length-1];
         int n= shifts.length;
         for(int i=n-2;i>=0;i--){
-            shifts[i]+=shifts[i+1];
-            shifts[i]%=26;
+            longShifts[i] = longShifts[i+1] + shifts[i];
         }
         char[] ch = s.toCharArray();
         for(int i=0;i<n;++i){
-            int x = (shifts[i] + ch[i]-97)%26 ;
+            long x = (longShifts[i] + ch[i]-97)%26 ;
             ch[i] = (char)(x+97);
         }
         return new String(ch);
