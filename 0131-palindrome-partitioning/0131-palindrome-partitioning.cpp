b633e910707e7169f44cce2d3f2197b4ac8d80f4
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-09-30 20:02:56 +0300

    Time: 35 ms (75.71%), Space: 53.4 MB (85.5%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,44 +1,39 @@
-#define all(s) (s).begin(),(s).end()
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
         int n = s.size();
         vector<vector<string>> ans;
-        for (int i = 0; i < (pow(2, n - 1)); i++) { ///
-            bitset<16> X(i);
-            string bts = X.to_string();
-            reverse(all(bts));
-            string tmp;
-            char c = bts[0];
-            tmp += s[0];
-            vector<string> subs;
-            for (int j = 1; j < n; ++j) {
-
-                if (bts[j] == c) {
-                    tmp += s[j];
-                } else {
-                    subs.push_back(tmp);
-                    tmp = "";
-                    tmp += s[j];
-                    c = bts[j];
+        vector<string> current;
+        
+        // Step 1: Precompute palindrome substrings using DP
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        
+        for (int len = 1; len <= n; ++len) {
+            for (int i = 0; i + len - 1 < n; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    if (len <= 2) isPal[i][j] = true;
+                    else isPal[i][j] = isPal[i+1][j-1];
                 }
             }
-
-            subs.push_back(tmp);
-            bool notAllOfThemArePalind = 1;
-            for (auto& it : subs) {
-                int sz = it.size();
-                for (int k = 0; k < (sz / 2); k++) {
-                    if (it[k] != it[sz - k - 1]) {
-                        notAllOfThemArePalind = 0;
-                        break;
-                    }
-                }
-            }
-            if (notAllOfThemArePalind)
-                ans.push_back(subs);
         }
-
+        
+        // Step 2: Backtracking with DP guidance
+        function<void(int)> dfs = [&](int start) {
+            if (start == n) {
+                ans.push_back(current);
+                return;
+            }
+            for (int end = start; end < n; ++end) {
+                if (isPal[start][end]) {
+                    current.push_back(s.substr(start, end - start + 1));
+                    dfs(end + 1);
+                    current.pop_back();
+                }
+            }
+        };
+        
+        dfs(0);
         return ans;
     }
 };
