66f658f8ceafb924f6d43baa6be809e3b6e752a7
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-10-04 18:33:58 +0300

    Time: 4 ms (66.41%), Space: 12.2 MB (55.44%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -18,33 +18,21 @@ public:
 
 
           // optimized approach
-          vector<int>pref(n+2);
-          vector<int>suff(n+2);
-          if(boxes[0]=='1')pref[0]=1;
-          
-          for(int i=1;i<n;++i){
-            int x = (boxes[i]=='1')?1:0;
-            pref[i]+=pref[i-1]+x;
-          }
-          for(int i=1;i<n;++i){
-            pref[i]+=pref[i-1];
-          }
-          
-          if(boxes[n-1]=='1')suff[n-1]=1;
-          
-          for(int i=n-2;i>=0;--i){
-            int x = (boxes[i]=='1')?1:0;
-            suff[i]+=suff[i+1]+x;
-          }
-          
-          for(int i=n-2;i>=0;--i){ 
-            suff[i]+=suff[i+1];
-          }
-          operations[0]+=suff[1];
-          for(int i=1;i<n;++i){
-            operations[i]=(pref[i-1]+suff[i+1]);
-          }
-        
+        int prefSum=0, doublePrefSum=0;
+        int suffixSum=0, doubleSuffixSum=0;
+        for(int i=0;i<n;++i){
+            // do the prefix sum
+            operations[i]+=doublePrefSum;
+            prefSum+=boxes[i]-'0';
+            doublePrefSum +=prefSum; 
+            // do the suffix fum
+            
+            int j=n-i-1;
+            operations[j]+=doubleSuffixSum;
+            suffixSum+=boxes[j]-'0';
+            doubleSuffixSum +=suffixSum;
+        }
+
         return operations;
     }
 };
