df0ca945acd75277cc20528dec77e3284bca21b8
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-10-04 13:56:22 +0300

    Time: 0 ms (100%), Space: 8.2 MB (51.6%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -5,7 +5,6 @@ public:
         for(char c: word){
             cap+=(isupper(c)?1:0);
         }
-        
         return (cap==word.size() or cap==0 or ( cap==1 and isupper(word[0])) );
     }
 };
