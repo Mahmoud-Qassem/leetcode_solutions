56da4f7ed26d6340eefba1ba73e3964144212785
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-10-04 18:24:55 +0300

    Time: 0 ms (100%), Space: 12.6 MB (36.15%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,19 +1,50 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        // brute force approach
         int n= boxes.size();
         vector<int>operations(n);
-        for(int i=0;i<n;++i){
-            int minMoves=0;
-            for(int j=0;j<n;++j){
-                if(i==j)continue;
-                if(boxes[j]=='1'){
-                    minMoves+=(abs(i-j));
-                }
-            }
-            operations[i]=minMoves;
-        }
+        
+//        // brute force approach
+//        for(int i=0;i<n;++i){
+//            int minMoves=0;
+//            for(int j=0;j<n;++j){
+//                if(i==j)continue;
+//                if(boxes[j]=='1'){
+//                    minMoves+=(abs(i-j));
+//                }
+//            }
+//            operations[i]=minMoves;
+//        }
+
+
+          // optimized approach
+          vector<int>pref(n+2);
+          vector<int>suff(n+2);
+          if(boxes[0]=='1')pref[0]=1;
+          
+          for(int i=1;i<n;++i){
+            int x = (boxes[i]=='1')?1:0;
+            pref[i]+=pref[i-1]+x;
+          }
+          for(int i=1;i<n;++i){
+            pref[i]+=pref[i-1];
+          }
+          
+          if(boxes[n-1]=='1')suff[n-1]=1;
+          
+          for(int i=n-2;i>=0;--i){
+            int x = (boxes[i]=='1')?1:0;
+            suff[i]+=suff[i+1]+x;
+          }
+          
+          for(int i=n-2;i>=0;--i){ 
+            suff[i]+=suff[i+1];
+          }
+          operations[0]+=suff[1];
+          for(int i=1;i<n;++i){
+            operations[i]=(pref[i-1]+suff[i+1]);
+          }
+        
         return operations;
     }
 };
