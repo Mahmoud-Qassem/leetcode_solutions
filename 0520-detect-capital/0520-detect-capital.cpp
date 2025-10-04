cc03d32752fde95109af82456fcd13c3c4489de6
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-10-04 13:55:52 +0300

    Time: 0 ms (100%), Space: 8.2 MB (20.35%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cap = 0;
+        for(char c: word){
+            cap+=(isupper(c)?1:0);
+        }
+        
+        return (cap==word.size() or cap==0 or ( cap==1 and isupper(word[0])) );
+    }
+};
