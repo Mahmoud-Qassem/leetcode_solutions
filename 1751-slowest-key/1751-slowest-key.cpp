9b1bd72847eb06344f0e650bf0efca932f31e58d
Mahmoud Qassem <96304564+Mahmoud-Qassem@users.noreply.github.com>
2025-10-07 22:14:22 +0300

    Time: 3 ms (5%), Space: 14.7 MB (84.69%) - LeetHub

--- 1751-slowest-key/1751-slowest-key.cpp
+++ 1751-slowest-key/1751-slowest-key.cpp
@@ -4,18 +4,17 @@ public:
     char slowestKey(vector<int>& releaseTimes, string keysPressed) {
         int mx=releaseTimes[0],n=releaseTimes.size();
         for(int i=1;i<n;++i)mx=max(mx,releaseTimes[i]-releaseTimes[i-1]);
-        cout<<mx<<endl;
-        string ans;
-        if(mx==releaseTimes[0])
-        ans+=(keysPressed[0]);
-
+        set<char>ans;
+        if(releaseTimes[0]==mx)
+        ans.insert(keysPressed[0]);
         for(int i=1;i<n;++i){
             if(releaseTimes[i]-releaseTimes[i-1]==mx){
-                ans+=(keysPressed[i]);
+                ans.insert(keysPressed[i]);
             }
         }
-        sort(ans.begin(),ans.end());
-        cout<<ans<<endl;
-        return ans[ans.size()-1];
+        auto it=ans.end();
+        it--;
+        char c=*(it);
+        return c;
     }
 };
